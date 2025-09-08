027f95a36d2482c84f0981b270401864715b1956
ayman f. ali <ayman4swd@gmail.com>
2025-09-08 20:51:56 +0300

    feat: Install and configure Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,20 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ShopController;
-use App\Http\Controllers\StoreController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', [HomeController::class,'index'])->name('index');
-
-Route::get('/shop', [ShopController::class, 'index'])->name('index');
-
-Route::get('/products', [StoreController::class, 'index']);
-Route::get('/product-details', [StoreController::class, 'productDetails']);
-Route::get('/about', [StoreController::class, 'about']);
-Route::get('/cart', [StoreController::class, 'cart']);
-Route::get('/contact', [StoreController::class, 'contact'])->name('contact-form');
-
-
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
-
-    Route::get('/products', function () {
-        return view('admin.products');
-    })->name('admin.products');
-
-    Route::get('/categories', function () {
-        return view('admin.categories');
-    })->name('admin.categories');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/dashboard/products', [ProductController::class, 'index'])->name('products.index');
-// Route::get('/dashboard/products/{id}', [ProductController::class, 'show']);
-Route::get('/dashboard/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/dashboard/products', [ProductController::class, 'store'])->name('product.store');
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('/dashboard/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/contact', [ContactController::class, 'send']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
