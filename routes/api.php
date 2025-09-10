6ef81462a5e0803493d469a9631bc73c6b994f58
ayman f. ali <ayman4swd@gmail.com>
2025-09-10 06:35:07 +0300

    feat: Secure API with Sanctum and rate limiting

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,15 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::prefix('v1')->group(function () {
-    Route::apiResource('products', ProductController::class);
-    Route::apiResource('categories', CategoryController::class);
+Route::middleware('throttle:api')->prefix('v1')->group(function () {
+    Route::apiResource('products', ProductController::class)->except(['edit', 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
+        Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    });
+    Route::apiResource('categories', CategoryController::class)->except(['edit', 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::put('categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    });
 });
