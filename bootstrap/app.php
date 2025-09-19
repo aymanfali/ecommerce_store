57279b318149e0aa2be88d4322d194c4900ad9e0
ayman f. ali <ayman4swd@gmail.com>
2025-09-19 20:56:23 +0300

    feat: Send email on critical exceptions via withExceptions method

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,35 @@
 <?php
 
+use App\Exceptions\ProductNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->report(function ($e) {
+            
+            if ($e instanceof ProductNotFoundException) {
+                // Send email to support
+
+                Mail::raw(
+                    "A ProductNotFoundException occurred:\n\nMessage: {$e->getMessage()}",
+                    function ($message) {
+                        $message->to('support@techleaders.com')
+                            ->subject('Product Not Found Exception Alert');
+                    }
+                );
+            }
+        });
     })->create();
